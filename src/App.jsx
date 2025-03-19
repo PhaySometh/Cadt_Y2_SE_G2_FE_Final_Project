b115d2c17d3a7b461f6e8693c2a4f68d5c9953f2
PhaySometh <phay.someth70@gmail.com>
2025-03-19 18:17:42 +0700

    move to new repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,23 @@
-import { useState } from 'react';
-import { Search } from 'lucide-react';
-import './App.css';
-import 'tailwindcss';
-import SearchBox from './components/SearchBox';
-import AppButton from './components/AppButton';
-import SortBtn from './components/SortBtn';
-import DropdownMenu from './components/DropdownMenu';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// pages
+import { Home } from './pages/Home';
+import { Product } from './pages/Product';
+import AboutUs from './pages/AboutUs';
+//component
+import { Layout } from './components/layout/layout';
+
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-      <>
-          <div className="bg-gray-200 p-5 rounded-lg">
-              <h1 className="text-black font-bold my-5">Chlart Task Manager</h1>
-              <div className="flex flex-col gap-5 items-center">
-                  <SearchBox />
-                  <div className="flex justify-center gap-5">
-                      <AppButton label="Add new task" />
-                      <SortBtn />
-                  </div>
-              </div>
-              <div className="flex flex-col gap-5 bg-gray-50 p-5 rounded-lg">
-                  <h2 className="text-black font-bold my-5 text-4xl">Your task</h2>
-                  <div className="w-full bg-black rounded-lg p-2 text-white flex justify-between items-center">
-                      <div>
-                          <h3 className="text-2xl">Task 1</h3>
-                      </div>
-                      <DropdownMenu /> {/* This should now be aligned with your task bar */}
-                  </div>
-              </div>
-          </div>
-      </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/about" element={<AboutUs />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
 
 export default App;
